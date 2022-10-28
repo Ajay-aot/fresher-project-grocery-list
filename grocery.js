2c5084061939cf225ba139cb4e492740703e049e
Ajay krishna <ajay.k@aot-technologies.com>
2022-10-28 10:49:35 +0530

    updated

--- grocery.js
+++ grocery.js
@@ -2,7 +2,7 @@ let alert = document.querySelector(".alert")
 let form = document.querySelector(".gro-form")
 let grocery = document.getElementById("gro")
 let submit = document.querySelector(".sub-btn")
-let container = document.querySelector(".grocery-containers")
+let container = document.querySelector(".grocery-container")
 let list = document.querySelector(".grocery-list ")
 let clear = document.querySelector (".clear-btn")
 
@@ -14,57 +14,35 @@ let editid = ""
 
 form.addEventListener("submit",adding)
 clear.addEventListener("click",clearitems)
-
+window.addEventListener("DOMContentLoaded",setupItems)
 
 
 function adding(e){
     e.preventDefault()
-    let gro_value = grocery.value
+    let value = grocery.value
     let id = new Date().getTime().toString()
     console.log(id)
 
 
-    if(gro_value && !editflag){
+    if(value && !editflag){
         // console.log("hai")
-        let element = document.createElement("div")
-
-        element.classList.add("grocery-item")
-
-        let atr = document.createAttribute("data-id")
-        atr.value = id
-        element.setAttributeNode(atr)
-
-
-        element.innerHTML= `<p class="title mb-0 ">${gro_value}</p>
-        <div class="btn-container">
-        <i class="bi bi-pencil-square edit-btn"></i>
-        <i class="bi bi-trash-fill delete-btn text-danger "></i>
-        </div>`
-
-
-        let deletebtn = element.querySelector(".delete-btn")
-        let editbtn = element.querySelector(".edit-btn")
-        
-        deletebtn.addEventListener("click",deletingItem)
-        editbtn.addEventListener("click",editingItem)
-
-
-        list.appendChild(element)
-
+       
+        createListitem(id,value)
         displayAlert("entered successfully","success")
+        container.classList.add("show-container")
 
-        addTolocalstorage(id,gro_value)
+        addTolocalstorage(id,value)
 
         Clear()
 
 
     }
-    else if(gro_value && editflag){
+    else if(value && editflag){
         console.log("editing")
-        editelement.innerHTML = gro_value
+        editelement.innerHTML = value
         displayAlert("Edited Successfully","success")
 
-        editLocalStorage(editid,gro_value)
+        editLocalStorage(editid,value)
         Clear()
     }
     else{
@@ -107,7 +85,10 @@ function clearitems()
     }
     displayAlert("List is empty","danger")
     Clear()
+
     localStorage.removeItem("list")
+    container.classList.remove("show-container");
+
 }
 
 function deletingItem(x)
@@ -117,9 +98,13 @@ function deletingItem(x)
     const id = ele.dataset.id
 
     list.removeChild(ele)
+    if(list.children.length == 0) {
+        container.classList.remove("show-container")
+    }
 
     displayAlert("Item Removed","danger")
     removeFromLocalStorage(id)
+    Clear()
 }
 
 
@@ -133,16 +118,17 @@ function editingItem(x)
     editflag = true
     editid = ele.dataset.id
     submit.textContent = "Edit"
+    
 
 }
 
 
 //function for local storage
-function addTolocalstorage(id,gro_value)
+function addTolocalstorage(id,value)
 {
 
 console.log("hai")
-let grocery = { id, gro_value }
+let grocery = { id, value }
 
 console.log(grocery)
 let items = getLocalStorage()
@@ -168,13 +154,13 @@ localStorage.setItem("list",JSON.stringify(items))
 
 
 
-function  editLocalStorage(id,gro_value){
+function  editLocalStorage(id,value){
 
  let items = getLocalStorage()
- items.items.map(function(item){
+ items = items.map(function(item){
     if(item.id === id){
 
-        item.value = gro_value
+        item.value = value
 
     }
     return item
@@ -187,4 +173,46 @@ function  editLocalStorage(id,gro_value){
 function getLocalStorage(){
     return localStorage.getItem("list")
     ? JSON.parse(localStorage.getItem("list")) : []
+}
+
+
+function setupItems(){
+
+    let items = getLocalStorage()
+    if(items.length > 0){
+    items.forEach(function(item){
+        createListitem(item.id, item.value)
+    })
+    }
+
+}
+
+
+function createListitem(id,value)
+{
+    let element = document.createElement("div")
+
+    element.classList.add("grocery-item")
+
+    let atr = document.createAttribute("data-id")
+    atr.value = id
+    element.setAttributeNode(atr)
+
+
+    element.innerHTML= `<p class="title mb-0 ">${value}</p>
+    <div class="btn-container">
+    <i class="bi bi-pencil-square edit-btn"></i>
+    <i class="bi bi-trash-fill delete-btn text-danger "></i>
+    </div>`
+
+
+    let deletebtn = element.querySelector(".delete-btn")
+    let editbtn = element.querySelector(".edit-btn")
+    
+    deletebtn.addEventListener("click",deletingItem)
+    editbtn.addEventListener("click",editingItem)
+
+
+    list.appendChild(element)
+    container.classList.add("show-container")
 }
